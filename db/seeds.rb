e07bc9a7e6bb3f0d0f357cca7932bc539927870a
hoangmy121097 <hoangmy121097@gmail.com>
2019-04-18 11:28:56 +0700

    update 2

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Product.create(title: '	Description	Size	Color	Image url	Price	
+Lightweight Jacket',
+description:
+%{<p>
+<em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+</p>},
+image_url: '1.jpg',
+price: 26.00)
