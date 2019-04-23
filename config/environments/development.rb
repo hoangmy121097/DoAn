87a5cc68bc011002cbe87b66feacb52d659668e3
hoangmy121097 <hoangmy121097@gmail.com>
2019-04-23 21:16:09 +0700

    update login, logout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'domain.com',
+    :user_name => 'hoangmy121097@gmail.com',
+    :password => 'Aa123456!',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
-Product.create(title: '	Description	Size	Color	Image url	Price	
-Lightweight Jacket',
-description:
-%{<p>
-<em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
-</p>},
-image_url: '1.jpg',
-price: 26.00)
+Product.create(
+    title: 'Lightweight Jacket',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-01.jpg',
+    price: 40.00)
+Product.create(
+    title: 'Esprit Ruffle Shirt',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-02.jpg',
+    price: 33.00)
+Product.create(
+    title: 'Femme T-Shirt In Stripe',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-03.jpg',
+    price: 14.00)
+Product.create(
+    title: 'Only Check Trouser',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-04.jpg',
+    price: 36.00)
+Product.create(
+    title: 'Shirt in Stretch Cotton',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-05.jpg',
+    price: 20.00)
+Product.create(
+    title: 'Pieces Metallic Printed',
+    description:%{<p><em>Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.</p>},
+    image_url: 'product-06.jpg',
+    price: 42.00)
