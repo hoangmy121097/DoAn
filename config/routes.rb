239b807f0f2f1e32910f24b79e1176efb9777f24
rosiee2612 <hongphuong261297@gmail.com>
2019-05-01 22:17:12 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  
   resources :line_items
   resources :carts
   get 'store/index'
   devise_for :users
   resources :products
+    
   root to: 'store#index'
 
   authenticated :user do
@@ -13,4 +15,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root 'devise/sessions#new'
   end
+  resources :abouts
+  root 'abouts#index'
 end
+
